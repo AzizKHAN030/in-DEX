35d9cd8e620a0456d8676a853a6130fcf96fbe09
Azizbek <arasulov0066@gmail.com>
2021-01-29 17:24:40 +0500

    finish

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ $(window).on("scroll", function () {
     $(".main-logo").toggleClass("logo_2", $(this).scrollTop() > $(window).height()*0.5);
     setInterval(function () {
         if ($(".main-logo").hasClass("logo_2")) {
-            $(".logo_2").attr("src", "images/logo_655.png");
+            $(".logo_2").attr("src", "images/logo_white.png");
         } else {
-            $(".main-logo").attr("src", "images/logo_03.png");
+            $(".main-logo").attr("src", "images/logo.png");
         }
     }, 100);
 
@@ -19,3 +19,11 @@ $(window).on("scroll", function () {
     $(".navbar-toggler").toggleClass("custom-toggler", $(this).scrollTop() > $(window).height()*0.5);
 });
 
+$(document).ready(function(){
+    $("#navLinks").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top-65}, 1000);
+    });
+});
