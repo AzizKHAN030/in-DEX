e0dde52f8763894014b7ebf11f4e6db696d4f800
Azizbek <arasulov0066@gmail.com>
2021-01-30 21:40:02 +0500

    validation modified

--- js/main.js
+++ js/main.js
@@ -27,19 +27,39 @@ $(document).ready(function(){
         $('body,html').animate({scrollTop: top-65}, 1000);
     });
 });
+function isCharacterALetter(char) {
+    return (/[a-zA-Zа-яёА-яЁ]/).test(char)
+  }
 function validate() {
     let name=document.forms["contactForm"]["user_name"].value;
+    let b=0, c=0;
     if (name==""){
-        alert("Укажите Ваше имя");
+        alert("Введите Ваше имя");
         return false;
     }
-    if(name.length>25 || /\d/.test(name)){
-        alert("Проверьте правописание имени");
+    for (let i=0; i<name.length;i++){
+     if(isCharacterALetter(name[i])){
+         c++;
+     }
+     if(name[i]==" "){
+         b++;
+     }
+ }
+    if(c+b!=name.length){
+        alert("Имя не должно содержать символы");
+        return false;
+    }
+    else if(b>=1 && c==0){
+        alert("Введите Ваше имя");
+        return false;
+    }
+    else if(c<=2){
+        alert("Проверьте имя");
         return false;
     }
     let phoneNum=document.forms["contactForm"]["user_phone"].value; 
-    if(phoneNum=="" || phoneNum.lenght>12 || phoneNum.length<9 || /\D/.test(phoneNum)){
-        alert("Введите правильный формат номера!");
+    if(phoneNum=="" || phoneNum==" " || phoneNum.lenght>12 || phoneNum.length<9 || /\D/.test(phoneNum)){
+        alert("Введите правильный формат номера");
         return false;
     }
 }
